1288c98a3d8377725ffece356cf49ca25116f31c
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-01 14:38:25 +0300

    search server

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -9,18 +9,15 @@
 using namespace std;
 
 int main() {
-	SearchServer search_server("and with"s);
-	search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
-	search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(3, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(4, "funny pet and curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(5, "funny funny pet and nasty nasty rat"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(6, "funny pet and not very nasty rat"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(7, "very nasty rat and not very funny pet"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(8, "pet with rat and rat and rat"s, DocumentStatus::ACTUAL, {1, 2});
-	search_server.AddDocument(9, "nasty rat with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
-	cout << "Before duplicates removed: "s << search_server.GetDocumentCount() << endl;
-	RemoveDuplicates(search_server);
-	cout << "After duplicates removed: "s << search_server.GetDocumentCount() << endl;
-}
+    SearchServer search_server("и в на"s);
 
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
+    search_server.AddDocument(3, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
+    search_server.AddDocument(5, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, {1, 1, 1});
+
+    vector<Document> result = search_server.FindTopDocuments("пушистый"s);
+    for (auto& item : result) {
+        std::cout << item << std::endl;
+    }
+}
